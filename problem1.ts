b11580452a04128f9eccf955dad179a5d5fdfebb
irfanurislam <mdirfanurislam@gmail.com>
2023-11-09 01:52:25 +0600

    first commit

---
+++ problem1.ts
@@ -0,0 +1,16 @@
+{
+    // problem1
+
+    type Add ={
+        name: string;
+        new: string;
+    }
+
+    const segulo :Add = {
+        name:'irfan',
+        new: 'yae'
+    }
+
+    console.log(segulo)
+
+}
